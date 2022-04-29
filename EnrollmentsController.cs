a935918dc341138cb7f232297b33ea6e2899507f
Sadvitha <84315215+Sadvi3103461@users.noreply.github.com>
2022-04-29 08:52:35 +0530

    Add files via upload

---
+++ EnrollmentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABCcoachingProject.Models
+{
+    public class EnrollmentsController : Controller
+    {
+        private ABCcoachingDBEntities db = new ABCcoachingDBEntities();
+
+        // GET: Enrollments
+        public async Task<ActionResult> Index()
+        {
+            var enrollments = db.Enrollments.Include(e => e.courseDetail).Include(e => e.student);
+            return View(await enrollments.ToListAsync());
+        }
+
+        // GET: Enrollments/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Create
+        public ActionResult Create()
+        {
+            ViewBag.courseId = new SelectList(db.courseDetails, "courseId", "courseName");
+            ViewBag.stuId = new SelectList(db.students, "stuId", "stuLastName");
+            return View();
+        }
+
+        // POST: Enrollments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "EnrollmentId,courseId,stuId")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Enrollments.Add(enrollment);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.courseId = new SelectList(db.courseDetails, "courseId", "courseName", enrollment.courseId);
+            ViewBag.stuId = new SelectList(db.students, "stuId", "stuLastName", enrollment.stuId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.courseId = new SelectList(db.courseDetails, "courseId", "courseName", enrollment.courseId);
+            ViewBag.stuId = new SelectList(db.students, "stuId", "stuLastName", enrollment.stuId);
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "EnrollmentId,courseId,stuId")] Enrollment enrollment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(enrollment).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.courseId = new SelectList(db.courseDetails, "courseId", "courseName", enrollment.courseId);
+            ViewBag.stuId = new SelectList(db.students, "stuId", "stuLastName", enrollment.stuId);
+            return View(enrollment);
+        }
+
+        // GET: Enrollments/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            if (enrollment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(enrollment);
+        }
+
+        // POST: Enrollments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Enrollment enrollment = await db.Enrollments.FindAsync(id);
+            db.Enrollments.Remove(enrollment);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
